3829ef830ffe88c92cf12b53d34ade295841cc1b
guilherme <guilherme@lourenco.guilherme>
2019-10-18 19:01:24 -0300

    first commit

---
+++ src/main/java/br/com/zup/backend/BackendZupApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendZupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendZupApplication.class, args);
+    }
+
+}
